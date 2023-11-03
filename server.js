271eba9b9d69c46fe52389dc56d56c8c5aa59f0d
Sanni-Damilola <sannifortune11@gmail.com>
2023-11-03 10:09:46 +0100

    start

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const fs = require("fs")
 // create an instanse of express
 const app = express()
 app.use(express.json())
+const port = 2001
 
 
 app.get("/", (req, res) => {
+    let myString = "Welcome to TodoList";
+let message = myString.charAt(0).toUpperCase() + myString.slice(1).toLowerCase();
     res.json({
-        message: "Welcome to TodoList"
+        message
     })
+})
+
+app.listen(port,() => {
+    console.log("App Is Listening To", port);
 })
