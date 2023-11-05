01d3932c8b152ecafda8dd3c66ade193b2e117d8
Sanni-Damilola <sannifortune11@gmail.com>
2023-11-05 10:47:30 +0100

    start

--- server.js
+++ server.js
@@ -1,50 +1,50 @@
-const express = require("express")
-const fs = require("fs")
+const express = require("express");
+const fs = require("fs");
 
 // create an instanse of express
-const app = express()
-app.use(express.json())
-const port = 2001
-
+const app = express();
+app.use(express.json());
+const port = 2001;
 
 app.get("/", (req, res) => {
-    let message = "Welcome to TodoList";
-    res.json({
-        message
-    })
-})
+  let message = "Welcome to TodoList";
+  res.json({
+    message,
+  });
+});
 
-const fnReaddatabase = ((req, res) => {
-    const database = fs.readFileSync("./user.json", "utf-8")
-    return JSON.parse(database)
-})
+const fnReaddatabase = (req, res) => {
+  const database = fs.readFileSync("./user.json", "utf-8");
+  return JSON.parse(database);
+};
 
 const fnWriteDataBase = (data) => {
-    fs.writeFileSync("./user.json", JSON.stringify(data))
-}
+  fs.writeFileSync("./user.json", JSON.stringify(data));
+};
 
 // get all users
 app.get("/user", (req, res) => {
-    const user = fnReaddatabase()
-    res.json({
-        user,
-    })
-})
+  const user = fnReaddatabase();
+  res.json({
+    user,
+  });
+});
 
 // get one user
-app.get("/user/:id", (req, res) => {
-    const database = fnReaddatabase()
-    const {userID} = req.params
-    console.log("db", database.user);
-})
+app.get("/user/:userID", (req, res) => {
+  const database = fnReaddatabase();
+  const { userID } = req.params;
+  console.log("user", userID);
+  console.log("db", database.user);
+});
 
 // 404 route
 app.get("*", (req, res) => {
-    const getRoute = req.originalUrl()
-    res.json({
-        message: `${getRoute} Not FOund`,
-    })
-})
-app.listen(port,() => {
-    console.log("App Is Listening To", port);
-})
+  const getRoute = req.originalUrl();
+  res.json({
+    message: `${getRoute} Not FOund`,
+  });
+});
+app.listen(port, () => {
+  console.log("App Is Listening To", port);
+});
